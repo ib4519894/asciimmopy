b1dc6c1423fcd00214fe71743c6291b8a9b9c1cc
ib4519894 <ib4519894@lpssonline.com>
2021-09-08 15:02:53 +0000

    Change map to use Entity points instead of Str points

--- core/map.py
+++ core/map.py
@@ -1,31 +1,32 @@
-
+from core import entity
 class Map:
-    def __init__(self, width, height, default="#"):
+    def __init__(self, width, height, empty_char="#"):
         self._width = width
         self._height = height
-        self._default = default
+        self._default = empty_char
         self._grid = self._make_grid(self._width, self._height, self._default)
 
     def _make_grid(self, width, height, char):
-        row = [char for x in range(width)]
-        grid = [row for y in range(height)]
+        grid = [[entity.Entity("empty", x, y, "#", True) for x in range(width)] for y in range(height)]
         return grid
     
     def get_map(self):
         out = ""
         for row in self._grid:
             for point in row:
-                out = f"{out} {point}"
+                out = f"{out} {point.get_char()}"
             out = f"{out}\n"
         return out
 
-    def set_point(self, x_pos, y_pos, value):
+    def set_point(self, x_pos, y_pos, entity):
         out = []
         for y, old_row in enumerate(self._grid):
             row = []
             for x, old_value in enumerate(old_row):
                 if x == x_pos and y == y_pos:
-                    row.append(value)
+                    row.append(entity)
+                    entity.set_x_pos(x_pos)
+                    entity.set_y_pos(y_pos)
                 else:
                     row.append(old_value)
             out.append(row)
