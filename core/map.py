d96463868d7b7bd43f1ec55eb82c50d3b68e3cfa
ib4519894 <ib4519894@lpssonline.com>
2021-09-09 14:18:52 +0000

    Undo part of commit b1dc6c14 (use entity instead of str for points)

--- core/map.py
+++ core/map.py
@@ -7,26 +7,24 @@ class Map:
         self._grid = self._make_grid(self._width, self._height, self._default)
 
     def _make_grid(self, width, height, char):
-        grid = [[entity.Entity("empty", x, y, "#", True) for x in range(width)] for y in range(height)]
+        grid = [[char for x in range(width)] for y in range(height)]
         return grid
     
     def get_map(self):
         out = ""
         for row in self._grid:
             for point in row:
-                out = f"{out} {point.get_char()}"
+                out = f"{out} {point}"
             out = f"{out}\n"
         return out
 
-    def set_point(self, x_pos, y_pos, entity):
+    def _set_point(self, x_pos, y_pos, char):
         out = []
         for y, old_row in enumerate(self._grid):
             row = []
             for x, old_value in enumerate(old_row):
                 if x == x_pos and y == y_pos:
-                    row.append(entity)
-                    entity.set_x_pos(x_pos)
-                    entity.set_y_pos(y_pos)
+                    row.append(char)
                 else:
                     row.append(old_value)
             out.append(row)
