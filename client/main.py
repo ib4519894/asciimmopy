c4d149300015a7e3fb7096f92e0af05bd8136ad1
ib4519894 <ib4519894@lpssonline.com>
2021-09-09 14:45:42 +0000

    Add 4 directional movement on map to client

--- client/main.py
+++ client/main.py
@@ -1,8 +1,52 @@
-import core
+import core, os
+
+class Player(core.entity.Entity):
+    def __init__(self, name, start_x, start_y, map):
+        super().__init__(name, start_x, start_y, "@", map, False)
+    
+    def action(self):
+        valid = False
+        while not valid:
+            user_input = input("> ")
+            if user_input in "wasdh?help":
+                valid = True
+            else:
+                print("Invalid Input, try again or view help (?/h/help)")
+        
+        if user_input in "?h" or user_input == "help":
+            print("""
+            Options:
+                help (?/h/help) - view help
+                up (w) - move player up
+                down (s) - move player down
+                left (a) - move player left
+                right (d) - move player right
+            """)
+        elif user_input == "w":
+            self._map.set_point(self.get_x_pos(), self.get_y_pos(), self._map._default)
+            self.set_y_pos(self.get_y_pos() - 1)
+            self._map.set_point(self.get_x_pos(), self.get_y_pos(), "@")
+        elif user_input == "s":
+            self._map.set_point(self.get_x_pos(), self.get_y_pos(), self._map._default)
+            self.set_y_pos(self.get_y_pos() + 1)
+            self._map.set_point(self.get_x_pos(), self.get_y_pos(), "@")
+        elif user_input == "a":
+            self._map.set_point(self.get_x_pos(), self.get_y_pos(), self._map._default)
+            self.set_x_pos(self.get_x_pos() - 1)
+            self._map.set_point(self.get_x_pos(), self.get_y_pos(), "@")
+        elif user_input == "d":
+            self._map.set_point(self.get_x_pos(), self.get_y_pos(), self._map._default)
+            self.set_x_pos(self.get_x_pos() + 1)
+            self._map.set_point(self.get_x_pos(), self.get_y_pos(), "@")
 
 def main():
     print("client main")
     map = core.map.Map(20, 5)
-    print(map.get_map())
-    map.set_point(0,0,"@")
-    print(map.get_map())
+    player = Player("player", 2, 2, map)
+    map.actors.append(player)
+    map.set_point(player.get_x_pos(), player.get_y_pos(), player._char)
+
+    while True:
+        print(map.get_map())
+        map.turn()
+        os.system('cls' if os.name=='nt' else 'clear')
