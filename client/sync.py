7193674cf467797635ad591ae2b25c9eee185f71
ib4519894 <ib4519894@lpssonline.com>
2021-09-10 14:56:42 +0000

    Change use resp.read() in client sync

--- client/sync.py
+++ client/sync.py
@@ -4,6 +4,6 @@ async def sync(url, outgoing_data: dict) -> dict:
     async with aiohttp.ClientSession() as session:
         await session.post(url, json=outgoing_data)
         async with session.get(url) as resp:
-           incoming_data = await resp.json()
+           incoming_data = await resp.read()
 
     return incoming_data
