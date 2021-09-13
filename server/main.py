d3029cee218d569dcd982887b3b7c9628667467a
ib4519894 <ib4519894@lpssonline.com>
2021-09-13 15:21:31 +0000

    Improve placeholder server

--- server/main.py
+++ server/main.py
@@ -1,2 +1,17 @@
+from aiohttp import web
+import jsonpickle
+import core, server
+
 def main():
-    print("server main")
+    map = core.Map(10, 10, "#")
+    
+    routes = web.RouteTableDef()
+    
+    @routes.get("/")
+    async def get_sync(request):
+        data = {"map":jsonpickle.encode(map)}
+        return web.json_response(data)
+    
+    app = web.Application()
+    app.add_routes(routes)
+    web.run_app(app, port=9999)
